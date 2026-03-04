37f2c5ad82550011fc324fad89eb7eb998470b81
Moisessantos45 <santosxphdz34@gmail.com>
2026-03-04 17:02:17 -0600

    refactor: Rename form visibility state to  and introduce  prop to show form and scroll to top.

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -3,29 +3,35 @@ import ListPatientsFeature from "@/features/patients/components/ListPatientsFeat
 import { useState } from "react";
 
 const Admin = () => {
-  const [hiddenForm, setHiddenForm] = useState(false);
+  const [showForm, setShowForm] = useState(false);
+
+  const handleAdd = () => {
+    setShowForm(true);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <section className="flex flex-col md:flex-row gap-3 justify-center p-4">
       <button
         type="button"
         className="bg-indigo-600 uppercase font-bold mx-10 p-3 rounded-md text-white mb-10 md:hidden"
-        onClick={() => setHiddenForm(!hiddenForm)}
+        onClick={() => setShowForm(!showForm)}
       >
-        {" "}
-        {hiddenForm ? "Ocultar Form" : "Mostrar Form"}
+        {showForm ? "Ocultar Form" : "Mostrar Form"}
       </button>
       <div
         className={`${
-          hiddenForm ? "block" : "hidden"
+          showForm ? "block" : "hidden"
         } md:block md:w-1/2 lg:w-2/5`}
       >
         <FormPatientFeature />
       </div>
       <div className="md:w-1/2 lg:w-3/6">
-        <ListPatientsFeature />
+        <ListPatientsFeature onAdd={handleAdd} />
       </div>
     </section>
   );
 };
 
 export default Admin;
+
